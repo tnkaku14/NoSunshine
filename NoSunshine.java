6662c7b966eb7593668eb8fe91300d9e303ff3c9
tnkaku14 <tnkaku14@gmail.com>
2026-09-23 11:48:35 -0700

    Create NoSunshine.java

---
+++ NoSunshine.java
@@ -0,0 +1,76 @@
+public class NoSunshine {
+
+   public static void main(String[] args){
+   noSunshine();
+   System.out.println("It's not warm when she's away");
+   noSunshine();
+   System.out.println("And she's always gone too long");
+   anytime();
+   System.out.println();
+   
+   System.out.println("Wonder this time where she's gone");
+   System.out.println("Wonder if she's gone to stay");
+   noSunshine();
+   house();
+   anytime();
+   System.out.println();
+   
+   System.out.print("And ");
+   iK(", ");
+   iK(", ");
+   iK(", ");
+   iK("\n");
+   iK(", ");
+   iK(", ");
+   iK(", ");
+   iK(", ");
+   iK("\n");
+   iK(", ");
+   iK(", ");
+   iK(", ");
+   iK(", ");
+   iK("\n");
+   iK(", ");
+   iK(", ");
+   iK(", ");
+   iK(", ");
+   iK("\n");
+   iK(", ");
+   iK(", ");
+   iK(", ");
+   iK(", ");
+   iK("\n");
+   iK(", ");
+   iK("\n\n");
+   
+   System.out.println("Hey, I ought to leave young thing alone");
+   System.out.println("But ain't no sunshine when she's gone, whoa-whoa");
+   noSunshine();
+   System.out.println("Only darkness every day\n\n");
+   
+   noSunshine();
+   house();
+   System.out.println();
+   
+   anytime();
+   anytime();
+   anytime();
+   anytime();
+   }
+   
+   public static void noSunshine(){
+   System.out.println("Ain't no sunshine when she's gone");
+   }
+   public static void anytime(){
+   System.out.print("Anytime she goes away\n");
+   }
+   public static void iK(String punctuation){
+   System.out.print ("I know" + punctuation);
+   }
+   public static void iK(){
+   System.out.print ("I know");
+   }
+   public static void house(){
+   System.out.println("And this house just ain't no home");
+   }
+}
