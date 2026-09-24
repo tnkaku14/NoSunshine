394f4ef363d4a8715d0db79ab76828357292dda3
tnkaku14 <tnkaku14@gmail.com>
2026-09-24 12:37:41 -0700

    Update NoSunshine.java

--- NoSunshine.java
+++ NoSunshine.java
@@ -1,76 +1,50 @@
-public class NoSunshine {
-
-   public static void main(String[] args){
-   noSunshine();
-   System.out.println("It's not warm when she's away");
-   noSunshine();
-   System.out.println("And she's always gone too long");
-   anytime();
-   System.out.println();
-   
-   System.out.println("Wonder this time where she's gone");
-   System.out.println("Wonder if she's gone to stay");
-   noSunshine();
-   house();
-   anytime();
-   System.out.println();
-   
-   System.out.print("And ");
-   iK(", ");
-   iK(", ");
-   iK(", ");
-   iK("\n");
-   iK(", ");
-   iK(", ");
-   iK(", ");
-   iK(", ");
-   iK("\n");
-   iK(", ");
-   iK(", ");
-   iK(", ");
-   iK(", ");
-   iK("\n");
-   iK(", ");
-   iK(", ");
-   iK(", ");
-   iK(", ");
-   iK("\n");
-   iK(", ");
-   iK(", ");
-   iK(", ");
-   iK(", ");
-   iK("\n");
-   iK(", ");
-   iK("\n\n");
-   
-   System.out.println("Hey, I ought to leave young thing alone");
-   System.out.println("But ain't no sunshine when she's gone, whoa-whoa");
-   noSunshine();
-   System.out.println("Only darkness every day\n\n");
-   
-   noSunshine();
-   house();
-   System.out.println();
-   
-   anytime();
-   anytime();
-   anytime();
-   anytime();
-   }
-   
-   public static void noSunshine(){
-   System.out.println("Ain't no sunshine when she's gone");
-   }
-   public static void anytime(){
-   System.out.print("Anytime she goes away\n");
-   }
-   public static void iK(String punctuation){
-   System.out.print ("I know" + punctuation);
-   }
-   public static void iK(){
-   System.out.print ("I know");
-   }
-   public static void house(){
-   System.out.println("And this house just ain't no home");
-   }
+public class NoSunshine{
+public static void main(String[] args){
+a();
+p("It's not warm when she's away\n");
+a();
+p("And she's always gone too long\n");
+s();
+p("\n");
+p("Wonder this time where she's gone\nWonder if she's gone to stay\n");
+a();
+p("And this house just ain't no home\n");
+s();
+p("\n");
+p("And ");
+i();
+i();
+p("I know\n");
+for(int i=1;i<=4;i++){
+i();
+i();
+i();
+i();
+p("I know\n");
+}
+i();
+p("I know\n\n");
+p("Hey, I ought to leave young thing alone\n");
+p("But ain't no sunshine when she's gone, whoa-whoa\n");
+a();
+p("Only darkness every day\n\n");
+a();
+p("And this house just ain't no home\n\n");
+s();
+s();
+s();
+p("Anytime she goes away");
+}
+public static void p(String text){
+System.out.print(text);
+}
+public static void a(){
+System.out.println("Ain't no sunshine when she's gone");
+}
+public static void s(){
+System.out.println("Anytime she goes away");
+}
+public static void i(){
+System.out.print("I know, ");
+}
 }
